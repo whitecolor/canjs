4f8622996fb8022d1b00985fa10a86470270ba01
Jan Jorgensen <jandjorgensen@gmail.com>
2014-05-27 22:28:21 -0700

    added object and array observers

--- Gruntfile.js
+++ Gruntfile.js
@@ -209,7 +209,8 @@ module.exports = function (grunt) {
 			server: {
 				options: {
 					port: 8000,
-					base: '.'
+					base: '.',
+					keepalive: true
 				}
 			}
 		},
--- compute/dirty/watch.js
+++ compute/dirty/watch.js
@@ -1,13 +1,58 @@
-steal('can/compute', 'observejs', function(compute){
+steal('can', 'observejs', function(can){
 
-compute.watch = function(object, property){
-	var observer = new PathObserver(object, property);
-	var watchedCompute = can.compute(object[property]);
-	observer.open(function(newVal, oldVal){
-		return watchedCompute(newVal);
+can.compute.watch = function(object, property){
+		var observer = new PathObserver(object, property);
+		var watchedCompute = can.compute(object[property], {
+			get: function(){
+				observer.deliver();
+				return observer.value_;
+			},
+			set: function(newVal){
+				observer.setValue(newVal);
+			}
+		});
+		observer.open(function(newVal){
+			watchedCompute(newVal);
+		});
+
+		return watchedCompute;
+	}
+
+can.Map.watch = function(object){
+	var observer = new ObjectObserver(object);
+	var watchedMap = new can.Map(object);
+	observer.open(function(added, removed, changed){
+
+		Object.keys(added).forEach(function(property){
+			watchedMap.attr(property, changed[property]);
+		});
+
+		Object.keys(removed).forEach(function(property){
+			watchedMap.removeAttr(property);
+		});
+
+		Object.keys(changed).forEach(function(property){
+			watchedMap.attr(property, changed[property]);
+		});
 	});
 
-	return watchedCompute;
-};
+	return watchedMap;
+}
+
+can.List.watch = function(arr){
+	var observer = new ArrayObserver(arr);
+	var watchedArray = new can.List(arr);
+	observer.open(function(splices){
+		val = observer.value_;
+		splices.forEach(function(s){
+			var args = [s.index, s.removed.length].concat(arr.slice(s.index, s.addedCount));
+			can.List.prototype.splice.apply(watchedArray, args);
+		});
+	});
+
+	return watchedArray;
+}
+
 
 });
+
